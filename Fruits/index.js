630e09ce5f855bf4142415b5f4f146adaf6ef4d7
vinityadav <vinityadav885317@gmail.com>
2025-05-28 00:57:35 +0530

    export in repositories

--- Fruits/index.js
+++ Fruits/index.js
@@ -4,3 +4,4 @@ const banana=require("./banana");
 
 const fruits=[mango,orange,banana];
 module.exports=fruits;
+/// if we want's to export a repo files then we need to creat a index.js file which the entry point of that repository and then we reqire all file and creat the array of all file and then we export it.
